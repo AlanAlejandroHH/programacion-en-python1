99693ea555cccad9803997b56761d89a027968ef
AlanAlejandroHH <hiuga96@hotmail.com>
2016-12-14 16:33:23 -0600

    Add files via upload

---
+++ mini examen/mini examen 1.py
@@ -0,0 +1,12 @@
+Alan Alejandro Herrera Huerta
+#mini eaxamen 1
+
+def inversa (cadena):
+    invertida = ""
+    cont = len(cadena)
+    indice = -1
+    while cont >= 1:
+        invertida += cadena[indie]
+        indice = indice + (-1)
+        cont -= 1
+    return invertida
---
+++ mini examen/mini examen.py
@@ -0,0 +1,10 @@
+Alan Alejandro Herrera Huerta
+#mini eaxamen
+
+"print pida una palabra y muestrala inversa"
+
+def reverse(palabra):
+    if len(palabra)==1
+        return palabra
+    else:
+        return palabra[-1]+reverse(palabra[:-1])
