6751d2272f522eed04c72ab41cee4ac74f678101
AlanAlejandroHH <hiuga96@hotmail.com>
2016-12-14 15:58:13 -0600

    Add files via upload

---
+++ tarea 2/tarea 2.py
@@ -0,0 +1,7 @@
+Alan Alejandro Herrera Huerta
+#tarea 2
+banderazo=8.74
+def distancia(km):
+    n=(km/0.25)*1.84
+    dinero=n+banderazo
+    print dinero, "pesos"
---
+++ tarea 2/tarea2a.py
@@ -0,0 +1,7 @@
+Alan Alejandro Herrera Huerta
+#tarea 2a
+import math
+
+x = float(input("lado 1:"))
+y = float(input("lado 2:"))
+print "Hipotenusa igual a: " +str(math.sqrt((x*x)+(y*y)))
