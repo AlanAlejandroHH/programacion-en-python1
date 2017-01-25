cfdb6bc7cff20d1cb10aaa4970a79a2a199aab37
AlanAlejandroHH <hiuga96@hotmail.com>
2017-01-25 09:19:49 -0600

    Update tarea1.py

--- trabajos python 1/tarea1.py
+++ trabajos python 1/tarea1.py
@@ -1,8 +1,4 @@
-Python 2.7.12 (v2.7.12:d33e0cf91556, Jun 27 2016, 15:24:40) [MSC v.1500 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
-
-Alan Alejandro Herrera Huerta
+#Alan Alejandro Herrera Huerta
 #tarea 1 
 print "ingresa un numero"
 n = int(raw_input())
